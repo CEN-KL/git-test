ab803c270e53c8abcb947e59ace877092aad66a5
CEN-KL <78341030+CEN-KL@users.noreply.github.com>
2022-05-15 22:44:04 +0800

    change1
    change1

--- src/main/java/com/xxxcc/git/GitTest.java
+++ src/main/java/com/xxxcc/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("go hot-fix!!!");
         System.out.println("No, Stay here !");
         System.out.println("push ! push ! push");
+        System.out.println("Hello, I'm on GitHub !");
     }
 }
